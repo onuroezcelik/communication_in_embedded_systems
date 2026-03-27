76a476f1eeef0749cee8b351044d1e08f533e75c
onuroezcelik <onur.oezcelik@gmail.com>
2026-03-27 02:16:12 +0100

    project files before submission

---
+++ hardware_simulation/build/CMakeFiles/hardware.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/hardware_simulation/source/CAN.cpp" "CMakeFiles/hardware.dir/source/CAN.cpp.o" "gcc" "CMakeFiles/hardware.dir/source/CAN.cpp.o.d"
+  "/workspace/hardware_simulation/source/I2C.cpp" "CMakeFiles/hardware.dir/source/I2C.cpp.o" "gcc" "CMakeFiles/hardware.dir/source/I2C.cpp.o.d"
+  "/workspace/hardware_simulation/source/LIN.cpp" "CMakeFiles/hardware.dir/source/LIN.cpp.o" "gcc" "CMakeFiles/hardware.dir/source/LIN.cpp.o.d"
+  "/workspace/hardware_simulation/source/SPI.cpp" "CMakeFiles/hardware.dir/source/SPI.cpp.o" "gcc" "CMakeFiles/hardware.dir/source/SPI.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hardware_simulation/build/CMakeFiles/hardware.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hardware.dir/source/CAN.cpp.o"
+  "CMakeFiles/hardware.dir/source/CAN.cpp.o.d"
+  "CMakeFiles/hardware.dir/source/I2C.cpp.o"
+  "CMakeFiles/hardware.dir/source/I2C.cpp.o.d"
+  "CMakeFiles/hardware.dir/source/LIN.cpp.o"
+  "CMakeFiles/hardware.dir/source/LIN.cpp.o.d"
+  "CMakeFiles/hardware.dir/source/SPI.cpp.o"
+  "CMakeFiles/hardware.dir/source/SPI.cpp.o.d"
+  "libhardware.pdb"
+  "libhardware.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hardware.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
